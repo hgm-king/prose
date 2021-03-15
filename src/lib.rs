41002cf229f33a61fae0945303f8c7f0a954fc72
HGimself <hgmaxwellking@gmail.com>
2021-03-15 18:17:16 -0400

    making this become a library tool to parse markdown with

--- src/lib.rs
+++ src/lib.rs
@@ -1,195 +1,30 @@
-#![recursion_limit = "512"]
+pub mod parser;
+pub mod translator;
 
-// lib.rs
-use wasm_bindgen::prelude::*;
-use web_sys::{console, Node};
-use yew::virtual_dom::VNode;
-use yew::{Component, ComponentLink, Html, InputData, ShouldRender};
-use yew::prelude::*;
-mod markdown;
+pub type MarkdownText = Vec<MarkdownInline>;
 
-struct Model {
-    link: ComponentLink<Self>,
-    markdown: String,
-    value: String,
-    show: String,
+#[derive(Clone, Debug, PartialEq)]
+pub enum Markdown {
+    Heading(usize, MarkdownText),
+    OrderedList(Vec<MarkdownText>),
+    UnorderedList(Vec<MarkdownText>),
+    Line(MarkdownText),
+    Codeblock(String),
 }
 
-enum Msg {
-    GotInput(String),
-    Reset,
-    ToggleShow,
-    Clear,
+#[derive(Clone, Debug, PartialEq)]
+pub enum MarkdownInline {
+    Link(String, String),
+    Image(String, String),
+    InlineCode(String),
+    Bold(String),
+    Italic(String),
+    Plaintext(String),
 }
 
-const DEFAULT: &str = "# 🎭 Prose
-###### **Turning your markdown into lovely HTML!**
-Prose is here to let you draft up a document and watch it render in real time.
-
-Prose is here to let you draft up a document and watch it render in real time.
-If you want to use this HTML elsewhere, just click the button above to switch the view to raw, unrendered HTML. This way you can copy&paste is anywhere you'd like.
-When it is time to save your work, Prose gives you the ability download your document as an `.md` file.
-
-##### Built on the following tech:
-- 🦀[Rust](https://www.rust-lang.org/) as your typical programming language
-- 🕸[WASM](https://webassembly.org/) to run compiled Rust code in the browser
-- 🍟[Nom](https://github.com/Geal/nom) to parse the markdown into a Syntax Tree
-- 🌳[Yew](https://yew.rs/docs/) as the web framework
-
-#### How Do I Run This?
-Easy! You will need a few things.
-1. Install `rust` from the [rust-lang](https://www.rust-lang.org/tools/install) site.
-1. Install `wasm-pack` which is a crate from cargo. Just run `cargo install wasm-pack`.
-1. Run `wasm-pack build` to compile all of of the code into a wasm npm package.
-1. Install `npm` from the [npm](https://www.npmjs.com/get-npm) site.
-1. Run `npm run serve` in the `www` directory.
-1. Navigate to `https://localhost:8080` in your browser!
-
-#### Support
-###### Prose supports the following markdown structures:
-- Headers 1-6
-- Ordered Lists
-- Unordered Lists
-- Codeblocks (no specified language support)
-- **boldtext**
-- *italic text*
-- `inline_code`
-- Links
-- Images
-
-You may be asking: *What makes this better than any other markdown parser?*
-Well, this is implemented in a very performant systems programming language and is much much faster.
-Using WebAssembly, we have been able to compile this code into a format that runs super fast in the browser's JavaScript engine.
-You probably will see above a benchmark on how long it took to parse this. I have done my own measurements and found most tools are 30-50 times slower. Not a good look :(
-
-#### Coming Soon!
-- Bugfixes
-- Download Feature
-- New Markdown Flavors
-- In-Page Timer
-";
-
-pub struct Timer<'a> {
-    name: &'a str,
-}
-
-impl<'a> Timer<'a> {
-    pub fn new(name: &'a str) -> Timer<'a> {
-        console::time_with_label(name);
-        Timer { name }
+pub fn markdown(md: &str) -> String {
+    match parser::parse_markdown(md) {
+        Ok((_, m)) => translator::translate(m),
+        Err(_) => String::from("Sorry, this did not seem to work! Maybe your markdown was not well formed, have you hit [Enter] after your last line?"),
     }
-}
-
-impl<'a> Drop for Timer<'a> {
-    fn drop(&mut self) {
-        console::time_end_with_label(self.name);
-    }
-}
-
-impl Component for Model {
-    type Message = Msg;
-    type Properties = ();
-    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self {
-            link,
-            show: String::from("MARKDOWN"),
-            value: String::from(DEFAULT),
-            markdown: markdown::markdown::markdown(DEFAULT),
-        }
-    }
-
-    fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        match msg {
-            Msg::GotInput(new_value) => {
-                let _timer = Timer::new("Markdown::render");
-                self.markdown = markdown::markdown::markdown(&new_value);
-                self.value = new_value;
-            }
-            Msg::Reset => {
-                self.markdown = markdown::markdown::markdown(DEFAULT);
-                self.value = DEFAULT.to_string();
-            }
-            Msg::Clear => {
-                self.markdown = "".to_string();
-                self.value = "".to_string();
-            }
-            Msg::ToggleShow => {
-                if self.show == "HTML" {
-                    self.show = "MARKDOWN".to_string();
-                } else {
-                    self.show = "HTML".to_string();
-                }
-            }
-        }
-        true
-    }
-
-    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        // Should only return "true" if new properties are different to
-        // previously received properties.
-        // This component has no properties so we will always return "false".
-        false
-    }
-
-    fn view(&self) -> Html {
-        let rendered_markdown = {
-            let div = web_sys::window()
-                .unwrap()
-                .document()
-                .unwrap()
-                .create_element("div")
-                .unwrap();
-            div.set_inner_html(&self.markdown);
-            div
-        };
-
-        eprintln!("rendered_markdown: {:?}", rendered_markdown);
-        let node = Node::from(rendered_markdown);
-        let vnode = VNode::VRef(node);
-        eprintln!("rendered_markdown: {:?}", vnode);
-
-        html! {
-            <div class={"main"}>
-                <div class={"header"}>
-                    <h1>{"🎭 Prose"}</h1>
-                    <h6>{"Created by "}<a href={"https://github.com/HGHimself"}>{"HG King"}</a></h6>
-                </div>
-                <div class={"container"}>
-                    <div class={"left"}>
-                        <div class={"input"}>
-                            <textarea rows=100
-                                value=&self.value
-                                wrap={"off"}
-                                oninput=self.link.callback(|e: InputData| Msg::GotInput(e.value))
-                                placeholder="placeholder">
-                            </textarea>
-                        </div>
-                    </div>
-                    <div class={"right"}>
-                        <div class={"container"}>
-                            <button
-                                class={"success-button"}
-                                onclick=self.link.callback(|_| Msg::Reset)>{ "RESET" }</button>
-                            <button
-                                class={"info-button"}
-                                onclick=self.link.callback(|_| Msg::ToggleShow)>{ &self.show }</button>
-                            <button
-                                class={"error-button"}
-                                onclick=self.link.callback(|_| Msg::Clear)>{ "CLEAR" }</button>
-                        </div>
-                        <div class={"output"}>
-                            <div class={if &self.show == "MARKDOWN" { "x" } else { "hidden" }}>{vnode}</div>
-                            <div class={if &self.show == "HTML" { "x" } else { "hidden" }}>{&self.markdown}</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        }
-
-    }
-}
-
-#[wasm_bindgen(start)]
-pub fn run_app() {
-    App::<Model>::new().mount_to_body();
 }
---
+++ src/translator.rs
@@ -0,0 +1,221 @@
+use crate::Markdown;
+use crate::MarkdownInline;
+use crate::MarkdownText;
+
+pub fn translate(md: Vec<Markdown>) -> String {
+    md.iter()
+        .map(|bit| match bit {
+            Markdown::Heading(size, line) => translate_header(*size, line.to_vec()),
+            Markdown::UnorderedList(lines) => translate_unordered_list(lines.to_vec()),
+            Markdown::OrderedList(lines) => translate_ordered_list(lines.to_vec()),
+            Markdown::Codeblock(code) => translate_codeblock(code.to_string()),
+            Markdown::Line(line) => translate_line(line.to_vec()),
+        })
+        .collect::<Vec<String>>()
+        .join("")
+}
+
+fn translate_boldtext(boldtext: String) -> String {
+    format!("<b>{}</b>", boldtext)
+}
+
+fn translate_italic(italic: String) -> String {
+    format!("<i>{}</i>", italic)
+}
+
+fn translate_inline_code(code: String) -> String {
+    format!("<code>{}</code>", code)
+}
+
+fn translate_link(text: String, url: String) -> String {
+    format!("<a href=\"{}\">{}</a>", url, text)
+}
+
+fn translate_image(text: String, url: String) -> String {
+    format!("<img src=\"{}\" alt=\"{}\" />", url, text)
+}
+
+fn translate_list_elements(lines: Vec<MarkdownText>) -> String {
+    lines
+        .iter()
+        .map(|line| format!("<li>{}</li>", translate_text(line.to_vec())))
+        .collect::<Vec<String>>()
+        .join("")
+}
+
+fn translate_header(size: usize, text: MarkdownText) -> String {
+    format!("<h{}>{}</h{}>", size, translate_text(text), size)
+}
+
+fn translate_unordered_list(lines: Vec<MarkdownText>) -> String {
+    format!("<ul>{}</ul>", translate_list_elements(lines.to_vec()))
+}
+
+fn translate_ordered_list(lines: Vec<MarkdownText>) -> String {
+    format!("<ol>{}</ol>", translate_list_elements(lines.to_vec()))
+}
+
+fn translate_code(code: MarkdownText) -> String {
+    format!("<code>{}</code>", translate_text(code))
+}
+
+fn translate_codeblock(code: String) -> String {
+    format!("<code><pre>{}</pre></code>", code)
+}
+
+fn translate_line(text: MarkdownText) -> String {
+    let line = translate_text(text);
+    if line.len() > 0 {
+        format!("<p>{}</p>", line)
+    } else {
+        format!("{}", line)
+    }
+}
+
+fn translate_text(text: MarkdownText) -> String {
+    text.iter()
+        .map(|part| match part {
+            MarkdownInline::Bold(text) => translate_boldtext(text.to_string()),
+            MarkdownInline::Italic(text) => translate_italic(text.to_string()),
+            MarkdownInline::InlineCode(code) => translate_inline_code(code.to_string()),
+            MarkdownInline::Link(text, url) => translate_link(text.to_string(), url.to_string()),
+            MarkdownInline::Image(text, url) => translate_image(text.to_string(), url.to_string()),
+            MarkdownInline::Plaintext(text) => text.to_string(),
+        })
+        .collect::<Vec<String>>()
+        .join("")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_translate_boldtext() {
+        assert_eq!(
+            translate_boldtext(String::from("bold af")),
+            String::from("<b>bold af</b>")
+        );
+    }
+
+    #[test]
+    fn test_translate_italic() {
+        assert_eq!(
+            translate_italic(String::from("italic af")),
+            String::from("<i>italic af</i>")
+        );
+    }
+
+    #[test]
+    fn test_translate_inline_code() {
+        assert_eq!(
+            translate_inline_code(String::from("code af")),
+            String::from("<code>code af</code>")
+        );
+    }
+
+    #[test]
+    fn test_translate_link() {
+        assert_eq!(
+            translate_link(
+                String::from("click me!"),
+                String::from("https://github.com")
+            ),
+            String::from("<a href=\"https://github.com\">click me!</a>")
+        );
+    }
+
+    #[test]
+    fn test_translate_image() {
+        assert_eq!(
+            translate_image(String::from("alt text"), String::from("https://github.com")),
+            String::from("<img src=\"https://github.com\" alt=\"alt text\" />")
+        );
+    }
+
+    #[test]
+    fn test_translate_text() {
+        let x = translate_text(vec![
+            MarkdownInline::Plaintext(String::from(
+                "Foobar is a Python library for dealing with word pluralization.",
+            )),
+            MarkdownInline::Bold(String::from("bold")),
+            MarkdownInline::Italic(String::from("italic")),
+            MarkdownInline::InlineCode(String::from("code")),
+            MarkdownInline::Link(String::from("tag"), String::from("https://link.com")),
+            MarkdownInline::Image(String::from("tag"), String::from("https://link.com")),
+            MarkdownInline::Plaintext(String::from(". the end!")),
+        ]);
+        assert_eq!(x, String::from("Foobar is a Python library for dealing with word pluralization.<b>bold</b><i>italic</i><code>code</code><a href=\"https://link.com\">tag</a><img src=\"https://link.com\" alt=\"tag\" />. the end!"));
+        let x = translate_text(vec![]);
+        assert_eq!(x, String::from(""));
+    }
+
+    #[test]
+    fn test_translate_header() {
+        assert_eq!(
+            translate_header(1, vec![MarkdownInline::Plaintext(String::from("Foobar"))]),
+            String::from("<h1>Foobar</h1>")
+        );
+    }
+
+    #[test]
+    fn test_translate_list_elements() {
+        assert_eq!(
+            translate_list_elements(vec![
+                vec![MarkdownInline::Plaintext(String::from("Foobar"))],
+                vec![MarkdownInline::Plaintext(String::from("Foobar"))],
+                vec![MarkdownInline::Plaintext(String::from("Foobar"))],
+                vec![MarkdownInline::Plaintext(String::from("Foobar"))],
+            ]),
+            String::from("<li>Foobar</li><li>Foobar</li><li>Foobar</li><li>Foobar</li>")
+        );
+    }
+
+    #[test]
+    fn test_translate_unordered_list() {
+        assert_eq!(
+            translate_unordered_list(vec![
+                vec![MarkdownInline::Plaintext(String::from("Foobar"))],
+                vec![MarkdownInline::Plaintext(String::from("Foobar"))],
+                vec![MarkdownInline::Plaintext(String::from("Foobar"))],
+                vec![MarkdownInline::Plaintext(String::from("Foobar"))],
+            ]),
+            String::from("<ul><li>Foobar</li><li>Foobar</li><li>Foobar</li><li>Foobar</li></ul>")
+        );
+    }
+
+    #[test]
+    fn test_translate_ordered_list() {
+        assert_eq!(
+            translate_ordered_list(vec![
+                vec![MarkdownInline::Plaintext(String::from("Foobar"))],
+                vec![MarkdownInline::Plaintext(String::from("Foobar"))],
+                vec![MarkdownInline::Plaintext(String::from("Foobar"))],
+                vec![MarkdownInline::Plaintext(String::from("Foobar"))],
+            ]),
+            String::from("<ol><li>Foobar</li><li>Foobar</li><li>Foobar</li><li>Foobar</li></ol>")
+        );
+    }
+
+    #[test]
+    fn test_translate_codeblock() {
+        assert_eq!(
+			translate_codeblock(String::from("python\nimport foobar\n\nfoobar.pluralize(\'word\') # returns \'words\'\nfoobar.pluralize(\'goose\') # returns \'geese\'\nfoobar.singularize(\'phenomena\') # returns \'phenomenon\'\n")),
+			String::from("<code><pre>python\nimport foobar\n\nfoobar.pluralize(\'word\') # returns \'words\'\nfoobar.pluralize(\'goose\') # returns \'geese\'\nfoobar.singularize(\'phenomena\') # returns \'phenomenon\'\n</pre></code>")
+		);
+    }
+
+    #[test]
+    fn test_translate_line() {
+        assert_eq!(
+            translate_line(vec![
+                MarkdownInline::Plaintext(String::from("Foobar")),
+                MarkdownInline::Bold(String::from("Foobar")),
+                MarkdownInline::Italic(String::from("Foobar")),
+                MarkdownInline::InlineCode(String::from("Foobar")),
+            ]),
+            String::from("<p>Foobar<b>Foobar</b><i>Foobar</i><code>Foobar</code></p>")
+        );
+    }
+}
